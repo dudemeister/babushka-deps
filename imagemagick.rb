1e146f2e7dc20f999f3d6ac15669300bbb466f0c
Ali Jelveh <jelveh@web.de>
2010-07-21 13:13:41 +0200

    fixing imagemagick dep

--- imagemagick.rb
+++ imagemagick.rb
@@ -1,20 +1,12 @@
-dep 'imagemagick' do
-  requires 'pkg imagemagick'
-end
-
 dep 'libmagickwand-dev', :template => 'managed' do
-  installs {
-    via :apt, 'libmagickwand-dev'
-  }
   provides []
 end
 
 dep 'imagemagick', :template => 'managed' do
-  requires 'pkg libmagickwand-dev'
-  
-  installs {
-    via :apt, 'imagemagick'
-  }
-  
   provides 'identify'
+end
+
+dep 'imagemagick complete' do
+  requires 'libmagickwand-dev'
+  requires 'imagemagick'
 end
