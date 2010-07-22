7c4e346f9c9ef1fc6fe1515187994d1e8cd65abf
Ali Jelveh <jelveh@web.de>
2010-07-22 14:07:02 +0200

    make rabbitmq write out a pid

--- rabbitmq.rb
+++ rabbitmq.rb
@@ -1,1 +1,12 @@
 dep 'rabbitmq-server', :template => 'managed'
+
+# this one add a line to the init script so rabbitmq writes out a monit understandable pidfile
+# based on http://blog.joeygeiger.com/2009/02/25/rabbitmq-and-monit/
+dep 'rabbitmq-with-pid' do
+  met? {
+     grep(/rabbitmq\.pid/, '/etc/init.d/rabbitmq-server')
+  }
+  meet {
+    change_line("echo SUCCESS", "echo SUCCESS\nsed 's/.*,\(.*\)\}.*/\1/' /var/lib/rabbitmq/pids > /var/run/rabbitmq.pid", "/etc/init.d/rabbitmq-server")
+  }
+end
