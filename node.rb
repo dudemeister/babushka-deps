b61af5b54605d8c0ec201403aadee5073b0222aa
Ali Jelveh <jelveh@web.de>
2010-07-27 15:22:00 +0200

    fix for node dep

--- node.rb
+++ node.rb
@@ -1,6 +1,4 @@
 dep 'node.src' do
-  src 'node' do
-    source "http://nodejs.org/dist/node-v0.1.102.tar.gz"
-    provides 'node'
-  end
+  source "http://nodejs.org/dist/node-v0.1.102.tar.gz"
+  provides 'node'
 end
