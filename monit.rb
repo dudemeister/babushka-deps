75192395a515be51180bfbf7318d79a270259d25
Ali Jelveh <jelveh@web.de>
2010-08-30 16:25:49 +0200

    autostart monit

--- monit.rb
+++ monit.rb
@@ -1,1 +1,6 @@
 dep 'monit', :template => 'managed'
+
+dep "autostart monit" do
+  met? { !grep(/^[^#]*startup=0/, "/etc/default/monit") }
+  meet { change_line "startup=0", "startup=1", "/etc/default/monit" } 
+end
