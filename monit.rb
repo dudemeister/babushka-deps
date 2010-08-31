225dfe5b80a9f55bbe7e5d0c0b9cf2e900aa7e06
Ali Jelveh <jelveh@web.de>
2010-08-31 17:07:52 +0200

    bla

--- monit.rb
+++ monit.rb
@@ -1,10 +1,10 @@
 dep 'monit', :template => 'managed'
 
 dep "autostart monit" do
-  met? { !grep(/^[^#]*startup=0/, "/etc/default/monit") && File.exists?("/etc/init/monit") }
+  met? { !grep(/^[^#]*startup=0/, "/etc/default/monit") && File.exists?("/etc/init/monit.conf") }
   meet {
     change_line "startup=0", "startup=1", "/etc/default/monit"
-    unless(File.exists?("/etc/init/monit"))
+    unless(File.exists?("/etc/init/monit.conf"))
       render_erb 'monit/monit.erb', :to => '/etc/init/monit.conf', :sudo => true
     end
   } 
