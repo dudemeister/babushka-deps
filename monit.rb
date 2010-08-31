f58af8bbb920e58e354f1145bb93fe5bd727cfe7
Ali Jelveh <jelveh@web.de>
2010-08-31 17:02:02 +0200

    add upstart for monit

--- monit.rb
+++ monit.rb
@@ -1,6 +1,11 @@
 dep 'monit', :template => 'managed'
 
 dep "autostart monit" do
-  met? { !grep(/^[^#]*startup=0/, "/etc/default/monit") }
-  meet { change_line "startup=0", "startup=1", "/etc/default/monit" } 
+  met? { !grep(/^[^#]*startup=0/, "/etc/default/monit") && File.exists?("/etc/event.d/monit") }
+  meet {
+    change_line "startup=0", "startup=1", "/etc/default/monit"
+    unless(File.exists?("/etc/event.d/monit"))
+      render_erb 'monit/monit.erb', :to => '/etc/event.d/monit/monit', :sudo => true
+    end
+  } 
 end
