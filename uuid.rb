1cef40cfa7c8a3b4da9f183064dd646104a64d5d
Ali Jelveh <jelveh@web.de>
2010-09-22 17:26:38 +0200

    uuid stuff

---
+++ uuid.rb
@@ -0,0 +1,34 @@
+dep "uuid" do
+  met? {
+    which 'uuid'
+  }
+  meet {
+    in_dir('/tmp') { |path|
+      uuid_version = "uuid-1.6.2"
+      log_shell "downloading ossp uuid", "curl -LO ftp://ftp.ossp.org/pkg/lib/uuid/#{uuid_version}.tar.gz", {:spinner => true}
+      log_shell "expanding   ossp uuid", "tar xzf #{uuid_version}.tar.gz", {:spinner => true}
+      in_dir(uuid_version) { |path|
+        log_shell "configure ", "./configure"
+        log_shell "making    ", "make", {:spinner => true}
+        log_shell "installing", "make install", {:spinner => true, :sudo => true}
+      }
+    }
+  }
+end
+
+dep "uuid4r" do
+  requires 'uuid'
+  met? {
+    failable_shell("ruby -e \"require 'uuid4r'\"").stderr.empty?
+  }
+  meet {
+    in_dir('/tmp') { |path|
+      log_shell "getting uuid4r from github", "git clone http://github.com/skaes/uuid4r.git", {:spinner => true}
+      in_dir('uuid4r/ext') { |path|
+        log_shell "configure ", "ruby extconf.rb"
+        log_shell "making    ", "make", {:spinner => true}
+        log_shell "installing", "make install", {:spinner => true, :sudo => true}
+      }
+    }
+  }
+end
