56ab4eb038257131ddda1773ec06fa9f3de9ac47
Philip MacIver <philip@ivercore.com>
2010-09-27 19:46:15 +0200

    clean compilation

--- uuid.rb
+++ uuid.rb
@@ -26,6 +26,7 @@ dep "uuid4r" do
       log_shell "getting uuid4r from github", "git clone http://github.com/skaes/uuid4r.git", {:spinner => true}
       in_dir('uuid4r/ext') { |path|
         log_shell "configure ", "ruby extconf.rb"
+        log_shell "cleaning",   "make clean"
         log_shell "making    ", "make", {:spinner => true}
         log_shell "installing", "make install", {:spinner => true, :sudo => true}
       }
