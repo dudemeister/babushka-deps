1546e01b6d76d60c1863ab994516da85b90adf52
Ali Jelveh <jelveh@web.de>
2010-10-04 17:32:27 +0200

    minor changes to the monit foo

--- monit.rb
+++ monit.rb
@@ -4,6 +4,8 @@ dep "autostart monit" do
   met? { !grep(/^[^#]*startup=0/, "/etc/default/monit") && File.exists?("/etc/init/monit.conf") }
   meet {
     change_line "startup=0", "startup=1", "/etc/default/monit"
+    # remove existing monit startscripts
+    sudo("update-rc.d -f monit remove")
     unless(File.exists?("/etc/init/monit.conf"))
       render_erb 'monit/monit.erb', :to => '/etc/init/monit.conf', :sudo => true
     end
