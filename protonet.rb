41a914c458dcf7a49cdcf6d3ebf5366ffa87dac2
Ali Jelveh <jelveh@web.de>
2010-11-03 17:20:31 +0100

    foobar

--- protonet.rb
+++ protonet.rb
@@ -2,14 +2,16 @@ dep 'protonet babushka' do
   setup {
     define_var :deploy_key, :message => "Please enter your protonet license key"
   }
-  met? { File.exists?('/home/protonet/.babushka/sources/protonet/base.rb') }
+  met? { 
+    File.exists?('/home/protonet/.babushka/sources/protonet/base.rb')
+  }
   meet {
-    log_shell "adding babushka source", "mkdir -p /home/protonet/.babushka/sources/protonet"
     in_dir "/tmp" do
+      log_shell "cleaning   ", "rm babushka.tar.gz; rm -rf babushka"
       log_shell "downloading", "wget -O babushka.tar.gz http://releases.protonet.info/release/babushka/get/#{var :deploy_key}"
       if File.exists?("babushka.tar.gz")
         log_shell "unpacking  ", "tar xzf babushka.tar.gz"
-        log_shell "moving     ", "mkdir -p ~/.babushka/protonet;mv babushka ~/.babushka/protonet"
+        log_shell "moving     ", "mkdir -p /home/protonet/.babushka/protonet; mv babushka /home/protonet/.babushka/protonet"
       end
     end
   }
