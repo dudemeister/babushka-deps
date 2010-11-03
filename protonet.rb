697c87d42c5b93e8cdcbeeb81c3ae82dff6b222a
Ali Jelveh <jelveh@web.de>
2010-11-03 16:16:56 +0100

    adding the ability to get the protonet babushka stuff if you have the right key

---
+++ protonet.rb
@@ -0,0 +1,16 @@
+dep 'protonet babushka' do
+  setup {
+    define_var :deploy_key, :message => "Please enter your protonet license key"
+  }
+  met? { File.exists?('/home/protonet/.babushka/sources/protonet/base.rb') }
+  meet {
+    log_shell "adding babushka source", "mkdir -p /home/protonet/.babushka/sources/protonet"
+    in_dir "/tmp" do
+      log_shell "downloading", "wget http://releases.protonet.info/release/babushka/get/#{var :deploy_key}"
+      if File.exists?("babushka.tar.gz")
+        log_shell "unpacking  ", "tar xzf babushka.tar.gz"
+        log_shell "moving     ", "mkdir -p ~/.babushka/protonet;mv babushka ~/.babushka/protonet"
+      end
+    end
+  }
+end
