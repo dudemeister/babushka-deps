ace3c2bb6bb71e16c764bd4e4dde44314de01846
Ali Jelveh <jelveh@web.de>
2010-11-05 14:27:10 +0100

    trying something with uuid4r

--- uuid.rb
+++ uuid.rb
@@ -12,15 +12,16 @@ dep "uuid4r" do
     failable_shell("ruby -e \"require 'uuid4r';UUID4R::uuid(1)\"").stderr.empty?
   }
   meet {
-    in_dir('/tmp') { |path|
-      log_shell "clean up...", "rm -rf uuid4r"
-      log_shell "getting uuid4r from github", "git clone http://github.com/dudemeister/uuid4r.git", {:spinner => true}
-      in_dir('uuid4r/ext') { |path|
-        log_shell "configure ", "ruby extconf.rb"
-        log_shell "cleaning",   "make clean"
-        log_shell "making    ", "make", {:spinner => true}
-        log_shell "installing", "make install", {:spinner => true, :sudo => true}
-      }
-    }
+    log_shell "installing gem", "gem install fishman-uuid4r", {:spinner => true, :sudo => true}
+    # in_dir('/tmp') { |path|
+    #   log_shell "clean up...", "rm -rf uuid4r"
+    #   log_shell "getting uuid4r from github", "git clone http://github.com/dudemeister/uuid4r.git", {:spinner => true}
+    #   in_dir('uuid4r/ext') { |path|
+    #     log_shell "configure ", "ruby extconf.rb"
+    #     log_shell "cleaning",   "make clean"
+    #     log_shell "making    ", "make", {:spinner => true}
+    #     log_shell "installing", "make install", {:spinner => true, :sudo => true}
+    #   }
+    # }
   }
 end
