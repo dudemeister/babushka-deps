40306f03ee9aae4cc2f49ca8c2fca1632c61e716
Ali Jelveh <jelveh@web.de>
2010-11-09 15:10:01 +0100

    * more uuid changes

--- uuid.rb
+++ uuid.rb
@@ -15,16 +15,7 @@ dep "uuid4r" do
   met? {
     failable_shell("ruby -r rubygems -e \"require 'uuid4r';UUID4R::uuid(1)\"").stderr.empty?
   }
-  meet {
-    in_dir('/tmp') { |path|
-      log_shell "clean up...", "rm -rf uuid4r"
-      log_shell "getting uuid4r from github", "git clone http://github.com/skaes/uuid4r.git", {:spinner => true}
-      in_dir('uuid4r/ext') { |path|
-        log_shell "configure ", "ruby extconf.rb"
-        log_shell "cleaning",   "make clean"
-        log_shell "making    ", "make", {:spinner => true}
-        log_shell "installing", "make install", {:spinner => true, :sudo => true}
-      }
-    }
+  meet { 
+    log_shell "installing gem", "gem install fishman-uuid4r --no-rdoc --no-ri", {:spinner => true, :sudo => true}
   }
 end
