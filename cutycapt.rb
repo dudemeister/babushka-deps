d6d1a3aeb70b7ed6a7343373f9495ad0a2fc1738
Ali Jelveh <jelveh@web.de>
2011-01-06 14:03:03 +0100

    * more stuff (cutycapt, expect) and some changes to our passenger configs

---
+++ cutycapt.rb
@@ -0,0 +1,21 @@
+dep 'cutycapt' do
+  requires "git", "libqt4-webkit.managed", "libqt4-dev", "g++.managed"
+  met? { which("CutyCapt") }
+  meet {
+    in_dir "/tmp" do
+      log_shell "cleaning   ", "rm -rf cutycapt"
+      log_shell "downloading", "git clone https://github.com/jdecuyper/cutycapt.git"
+      if File.exists?("cutycapt")
+        in_dir "cutycapt/CutyCapt" do
+          log_shell "qmake      ", "qmake"
+          log_shell "make       ", "make"
+          log_shell "copy       ", "cp CutyCapt /usr/local/bin"
+        end
+      end
+    end
+  }
+end
+
+dep "libqt4-webkit.managed"
+dep "libqt4-dev.managed"
+dep "g++.managed"
