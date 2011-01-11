fe8d1e09a0e634efcc5fc6d48cd3d39457b92a44
Ali Jelveh <jelveh@web.de>
2011-01-11 22:59:51 +0100

    minor node.js changes

--- coffee.rb
+++ coffee.rb
@@ -1,14 +1,9 @@
 dep 'coffeescript.src' do
-  requires 'nodejs.src'
+  requires 'node.src'
   source 'git://github.com/jashkenas/coffee-script.git'
   provides 'coffee ~> 0.9.6'
 
   configure { true }
   build { shell "bin/cake build" }
   install { shell "bin/cake install" }
-end
-
-dep 'nodejs.src' do
-  source 'git://github.com/ry/node.git'
-  provides 'node', 'node-waf'
 end
--- node.rb
+++ node.rb
@@ -1,4 +1,4 @@
 dep 'node.src' do
-  source "http://nodejs.org/dist/node-v0.2.1.tar.gz"
-  provides 'node'
+  source "http://nodejs.org/dist/node-v0.2.6.tar.gz"
+  provides 'node', 'node-waf'
 end
