20a955072f6bb1be9901145eff4d9980217c9ac6
Ali Jelveh <jelveh@web.de>
2011-01-17 20:08:25 +0100

    * add graphicsmagick from source

--- graphicsmagick.rb
+++ graphicsmagick.rb
@@ -1,3 +1,8 @@
 dep 'graphicsmagick.managed' do
   provides 'gm'
 end
+
+dep 'graphicsmagick.src' do
+  source 'http://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/1.3.12/GraphicsMagick-1.3.12.tar.gz/download'
+  provides 'gm'
+end
