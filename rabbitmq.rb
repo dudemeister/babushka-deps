9f5ec73ab6606684fb33ba1b2a0c6230eb7c8355
Ali Jelveh <jelveh@web.de>
2011-08-22 13:10:23 +0200

    * rabbitmq updates

--- rabbitmq.rb
+++ rabbitmq.rb
@@ -21,3 +21,20 @@ dep 'rabbitmq.src' do
   source "http://www.rabbitmq.com/releases/rabbitmq-server/v2.5.1/rabbitmq-server-2.5.1.tar.gz"
   provides ["rabbitmq-server"]
 end
+
+dep 'rabbitmq remove' do
+  met? {
+    ["rabbitmq-activate-plugins", "rabbitmq-multi", "rabbitmqctl", "rabbitmq-deactivate-plugins", "rabbitmq-server"].all? do |cmd|
+      which(cmd)
+    end
+  }
+  meet {
+    ["rabbitmq-activate-plugins", "rabbitmq-multi", "rabbitmqctl", "rabbitmq-deactivate-plugins", "rabbitmq-server"].each do |cmd|
+      sudo("rm -rf `which #{cmd}`")
+    end
+  }
+end
+
+dep 'rabbitmq update' do
+  requires 'rabbitmq remove', 'rabbitmq.src'
+end
