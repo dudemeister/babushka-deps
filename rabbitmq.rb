f6023d1225ed5c67980dc26f669afd9a5e9bd1cd
Ali Jelveh <jelveh@web.de>
2011-08-22 15:10:07 +0200

    * restart rabbitmq

--- rabbitmq.rb
+++ rabbitmq.rb
@@ -11,8 +11,7 @@ dep 'rabbitmq-with-pid' do
       "echo SUCCESS\n$CONTROL status | grep \"pid,[0-9]*\" | grep -o \"[0-9]*\" > /var/run/rabbitmq.pid",
       "/etc/init.d/rabbitmq-server")
     # stop and restart the rabbitmq server to reflect changes
-    sudo("/etc/init.d/rabbitmq-server stop")
-    sudo("/etc/init.d/rabbitmq-server start")
+    sudo("/etc/init.d/rabbitmq-server restart")
   }
 end
 
