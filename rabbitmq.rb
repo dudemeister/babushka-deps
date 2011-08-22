532c28a109f22a6a3fa156a2ed0931872b79547d
Ali Jelveh <jelveh@web.de>
2011-08-22 15:19:42 +0200

    * some debugging rabbitmq update

--- rabbitmq.rb
+++ rabbitmq.rb
@@ -8,10 +8,10 @@ dep 'rabbitmq-with-pid' do
   }
   meet {
     change_line("echo SUCCESS",
-      "echo SUCCESS\n$CONTROL status | grep \"pid,[0-9]*\" | grep -o \"[0-9]*\" > /var/run/rabbitmq.pid",
+      "$CONTROL status | grep \"pid,[0-9]*\" | grep -o \"[0-9]*\" > /var/run/rabbitmq.pid\necho SUCCESS",
       "/etc/init.d/rabbitmq-server")
     # stop and restart the rabbitmq server to reflect changes
-    sudo("/etc/init.d/rabbitmq-server restart &")
+    sudo("/etc/init.d/rabbitmq-server restart")
   }
 end
 
@@ -26,7 +26,6 @@ dep 'rabbitmq.src' do
   process_source {
     sudo("dpkg -i --force-confnew rabbitmq-server_2.5.1-1_all.deb")
   }
-  # met? { shell("ruby --version") =~ /#{Regexp.escape("ruby 1.8.7 (2010-04-19 patchlevel 253) [x86_64-linux], MBARI 0x6770, Ruby Enterprise Edition 2010.02")}/ }
   provides ["rabbitmq-server"]
 end
 
