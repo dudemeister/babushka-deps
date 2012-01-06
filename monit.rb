26935fcb63ec353403f6095bec340c83308751ab
Ali Jelveh <jelveh@web.de>
2012-01-06 12:04:12 +0100

    * add latest monit source

--- monit.rb
+++ monit.rb
@@ -1,5 +1,11 @@
 dep 'monit', :template => 'managed'
 
+dep 'monit.src' do
+  source 'http://mmonit.com/monit/dist/monit-5.3.2.tar.gz'
+  configure_args "--enable-optimized"
+  provides ['monit']
+end
+
 dep "autostart monit" do
   met? { !grep(/^[^#]*startup=0/, "/etc/default/monit") && File.exists?("/etc/init/monit.conf") }
   meet {
