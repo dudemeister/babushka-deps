63a8804717c28625d9a23adff52514e7ebaa0402
Ali Jelveh <jelveh@web.de>
2012-04-17 10:33:57 +0200

    * handle mongodb security by only allowing local connection

--- mongodb.rb
+++ mongodb.rb
@@ -1,3 +1,13 @@
 dep "mongodb.managed" do
   provides ["mongod"]
 end
+
+dep "dudemeister:mongodb only local connections" do
+  met? {
+    grep(/bind_ip = 127.0.0.1/, "/etc/mongodb.conf")
+  }
+  meet {
+    sudo("bind_ip = 127.0.0.1' > /etc/mongodb.conf")
+    sudo("/etc/init.d/mongodb restart")
+  }
+end
