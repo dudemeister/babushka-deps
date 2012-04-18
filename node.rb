7db769950f89a3d43c5816740fa5830c744521b9
Ali Jelveh <jelveh@web.de>
2012-04-18 13:38:01 +0200

    * add node 0.6.6 dep for our admin server

--- node.rb
+++ node.rb
@@ -2,3 +2,8 @@ dep 'node.src' do
   source "http://nodejs.org/dist/v0.6.5/node-v0.6.5.tar.gz"
   provides 'node', 'node-waf'
 end
+
+dep 'node066.src' do
+  source "http://nodejs.org/dist/v0.6.6/node-v0.6.6.tar.gz"
+  provides 'node', 'node-waf'
+end
