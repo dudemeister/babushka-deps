bcae4e8ab31c40db1f79bc1c4e78114d3758f936
Ali Jelveh <jelveh@web.de>
2012-05-08 16:50:38 +0200

    * fix syntax error

--- bundler.rb
+++ bundler.rb
@@ -27,13 +27,15 @@ end
 dep 'app bundled' do
   requires 'deployed app', 'bundler.gem', 'db gem'
   met? { cd(var(:rails_root)) { shell 'bundle check', :log => true } }
-  meet { cd(var(:rails_root)) {
-    install_args = var(:rails_env) != 'production' ? '' : "--deployment --without 'development test'"
-    unless shell("bundle install #{install_args}", :log => true)
-      confirm("Try a `bundle update`") {
-        shell 'bundle update', :log => true
-      }
-    end
+  meet { 
+    cd(var(:rails_root)) {
+      install_args = var(:rails_env) != 'production' ? '' : "--deployment --without 'development test'"
+      unless shell("bundle install #{install_args}", :log => true)
+        confirm("Try a `bundle update`") {
+          shell 'bundle update', :log => true
+        }
+      end
+    }
   }
 end
 
