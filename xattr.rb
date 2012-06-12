4dd4586bf36b08f2c76c6c581bb283f3eb86e6c7
tiff <christopher.blum@spellboy.com>
2012-06-12 16:41:40 +0200

    Enable xattr

--- xattr.rb
+++ xattr.rb
@@ -1,3 +1,16 @@
 dep 'python-xattr.managed' do
   provides 'xattr'
 end
+
+dep 'enable xattr' do
+  met? {
+    !!sudo("mount").match("user_xattr")
+  }
+  
+  meet {
+    needle = Regexp.escape("errors=remount-ro")
+    replacement = "errors=remount-ro,user_xattr"
+    sudo("sed -i '' -e 's/#{needle}/#{replacement}/' '/etc/fstab'")
+    sudo("mount -o remount,user_xattr /")
+  }
+end
