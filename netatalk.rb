25500f04254188d739ba1a34fccdbc693190a6eb
Henning Thies <thies.henning@gmail.com>
2012-06-27 10:16:07 +0200

    compile newer version of netatalk with pam flag

--- netatalk.rb
+++ netatalk.rb
@@ -1,5 +1,5 @@
 dep "netatalk.complete" do
-  requires "mdns.complete", "libssl-dev.managed", "libacl1-dev.managed", "libwrap0-dev.managed", "libgcrypt11-dev.managed", "libdb4.8.managed", "libdb4.8-dev.managed", "cups.src", "netatalk.source", "enable timemachine volumes"
+  requires "mdns.complete", "libssl-dev.managed", "libacl1-dev.managed", "libwrap0-dev.managed", "libgcrypt11-dev.managed", "libdb4.8.managed", "libdb4.8-dev.managed", "libpam0g-dev.managed", "libpam-devperm.managed", "cups.src", "netatalk.source", "netatalk permissions", "enable timemachine volumes"
 end
 
 dep "libssl-dev.managed" do
@@ -26,21 +26,46 @@ dep "libdb4.8-dev.managed" do
   provides []
 end
 
+dep "libpam0g-dev.managed" do
+  provides []
+end
+
+dep "libpam-devperm.managed" do
+  provides []
+end
+
 dep "netatalk.source" do
   met? {
     which('netatalk-config')
   }
   meet {
     cd('/tmp') { |path|
-      log_shell "downloading netatalk", "curl -LO http://downloads.sourceforge.net/project/netatalk/netatalk/2.2.1/netatalk-2.2.1.tar.gz", {:spinner => true}
-      log_shell "expanding", "tar xzf netatalk-2.2.1.tar.gz", {:spinner => true}
+      log_shell "downloading netatalk", "curl -LO http://downloads.sourceforge.net/project/netatalk/netatalk/2.2.3/netatalk-2.2.3.tar.gz", {:spinner => true}
+      log_shell "expanding", "tar xzf netatalk-2.2.3.tar.gz", {:spinner => true}
       # hostapd needs to build in the hostapd dir
-      cd("netatalk-2.2.1") { |path|
-        log_shell "configuring", "./configure --enable-debian"
+      cd("netatalk-2.2.3") {
+        log_shell "configuring", "./configure --enable-debian --with-pam"
         log_shell "making", "make", {:spinner => true}
         log_shell "installing", "make install", {:spinner => true, :sudo => true}
       }
     }
+  }
+  
+end
+
+dep "netatalk permissions" do
+  def config_path
+    "/usr/local/etc/netatalk/AppleVolumes.default"
+  end
+  
+  met? {
+    grep(/":DEFAULT: options:upriv,usedots dperm:0770 fperm:0660 umask:0007"/, "#{config_path}")
+  }
+  meet{
+    old_defaults = ":DEFAULT: options:upriv,usedots"
+    new_defaults = ":DEFAULT: options:upriv,usedots dperm:0770 fperm:0660 umask:0007"
+    ruby = which("ruby")
+    sudo("#{ruby} -pi -e \"gsub(/^#{old_defaults}$/, '#{new_defaults}')\" #{config_path}")    
   }
 end
 
@@ -48,10 +73,6 @@ dep "enable timemachine volumes" do
   
   def config_path
     "/usr/local/etc/netatalk/AppleVolumes.default"
-  end
-  
-  def section_name
-    "home_shares"
   end
   
   met? {
