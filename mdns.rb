6bda1cb366ea0e85d3ce58a3de416d68072562f7
Henning Thies <thies.henning@gmail.com>
2012-07-24 12:28:05 +0200

    add "avahi enable reflector" dep to enable bonjour across multiple subnets

--- mdns.rb
+++ mdns.rb
@@ -22,3 +22,13 @@ end
 dep "mdns.complete" do
   requires "mdns.managed", "mdns-scan.managed", "avahi-utils.managed", "libavahi-common-dev.managed", "libavahi-compat-libdnssd1.managed", "libavahi-compat-libdnssd-dev.managed"
 end
+
+dep "avahi enable reflector" do
+  change_line "#enable-reflector=no", "enable-reflector=yes", "/etc/avahi/avahi-daemon.conf"
+  sudo "/etc/init.d/avahi-daemon restart"
+end
+
+dep "avahi disable reflector" do
+  change_line "enable-reflector=yes", "#enable-reflector=no", "/etc/avahi/avahi-daemon.conf"
+  sudo "/etc/init.d/avahi-daemon restart"
+end
