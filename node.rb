a99a021e9e5e2580bb765023dbb3db5d29622700
Lee Hambley <lee.hambley@gmail.com>
2013-03-19 15:03:15 +0100

    Dep to upgrade npm

--- node.rb
+++ node.rb
@@ -3,6 +3,11 @@ dep 'node.src' do
   provides 'node', 'node-waf'
 end
 
+dep 'npm update' do
+  met? { eval(shell('npm version'))[:npm].to_i > 1.1 }
+  meet { shell('npm update npm -g')                  }
+end
+
 dep 'node066.src' do
   source "http://nodejs.org/dist/v0.6.6/node-v0.6.6.tar.gz"
   provides 'node', 'node-waf'
