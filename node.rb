acaaa20006a6819206f10f4414cf7b09709c1e64
Lee Hambley <lee.hambley@gmail.com>
2013-03-19 15:29:46 +0100

    Revert commits c2b1558, ec6b861 and a99a021

--- node.rb
+++ node.rb
@@ -3,11 +3,6 @@ dep 'node.src' do
   provides 'node', 'node-waf'
 end
 
-dep 'npm update' do
-  met? { shell('npm --version').to_f > 1.1               }
-  meet { log_shell("Upgrading NPM", 'npm update npm -g') }
-end
-
 dep 'node066.src' do
   source "http://nodejs.org/dist/v0.6.6/node-v0.6.6.tar.gz"
   provides 'node', 'node-waf'
