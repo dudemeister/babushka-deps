c55102968ca4578b35d7a0840f10ba54b7bd0a0e
Benjamin Behr <benny@digitalbehr.de>
2013-03-27 15:48:33 +0100

    prepare the node isntallation
    Conflicts:
    	node.rb

--- node.rb
+++ node.rb
@@ -7,3 +7,22 @@ dep 'node066.src' do
   source "http://nodejs.org/dist/v0.6.6/node-v0.6.6.tar.gz"
   provides 'node', 'node-waf'
 end
+
+dep 'nvm' do
+  met? {
+    system("nvm")
+  }
+  meet {
+    log_shell "installing nvm", "curl https://raw.github.com/creationix/nvm/master/install.sh | sh"
+    `. ~/nvm/nvm.sh`
+  }
+end
+
+dep 'nvm node0101' do
+  met? {
+    `node -v`.strip == 'v0.10.1'
+  }
+  meet {
+    log_shell "install node v0.10.1", "nvm install 0.10.1"
+  }
+end
